23fca2f1a309aa52ebd94b610cc15b4c70e4f135
Arsen Poteriaiko <72805407+niggvard@users.noreply.github.com>
2025-07-16 09:14:08 +0300

    Update launch.ps1

--- launch.ps1
+++ launch.ps1
@@ -2,7 +2,7 @@
 $exePath = "$env:TEMP\chatclient.exe"
 
 # Завантаження base64-коду
-$exe64 = (Invoke-WebRequest "https://raw.githubusercontent.com/niggvard/ChatTest/refs/heads/main/launch.ps1?token=GHSAT0AAAAAADG26J2G46ZWMALTKMNLAS5S2DXIK7Q").Content
+$exe64 = (Invoke-WebRequest "https://raw.githubusercontent.com/niggvard/ChatTest/refs/heads/main/chatclient.exe.b64?token=GHSAT0AAAAAADG26J2GGMZD63QVZ6E4HKSE2DXINLQ").Content
 
 # Декодування і запис в файл
 [IO.File]::WriteAllBytes($exePath, [Convert]::FromBase64String($exe64))
